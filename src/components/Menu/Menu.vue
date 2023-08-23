bc05ede00cd3a8d3fb40db2e8d9198175e45811a
Helena Delpizzo <hdelpizzo@gmail.com>
2023-08-23 12:08:24 -0300

    criacao da pasta components

---
+++ src/components/Menu/Menu.vue
@@ -0,0 +1,47 @@
+<template>
+        <nav>
+        <ul class="menu">
+        <router-link to="/" class="item-menu">
+            <li>
+                <img width="50" height="50" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>
+                <h2>Home</h2>
+            </li>
+        </router-link>
+        
+        <router-link to="/talentos/novo" class="item-menu">
+            <li>
+                <img width="50" height="50" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>
+                <h2>Cadastro</h2>
+            </li>
+        </router-link>
+        <router-link to="/sobre" class="item-menu">
+            <li>
+                <img width="50" height="50" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>
+                <h2>Sobre</h2>
+            </li>
+        </router-link>
+        </ul>
+    </nav>
+</template>
+
+<style scoped>
+.menu {
+    display: flex;
+    justify-content: space-between;
+    list-style: none;
+}
+.item-menu {
+    display: flex;
+    border: 1px solid black;
+    border-radius: 6px;
+    padding: 5px;
+    width: 33%;
+}
+.item-menu li {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    gap: 10px;
+    width: 33%;
+}
+</style>
--- src/views/Home/Home.vue
+++ src/views/Home/Home.vue
@@ -1,27 +1,4 @@
 <template>
-    <nav>
-        <ul class="menu">
-        <router-link to="/">
-            <li class="item-menu">
-                <img width="50" height="50" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>
-                <h2>Home</h2>
-            </li>
-        </router-link>
-        
-        <router-link to="/talentos/novo">
-            <li class="item-menu">
-                <img width="50" height="50" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>
-                <h2>Cadastro</h2>
-            </li>
-        </router-link>
-        <router-link to="/sobre">
-            <li class="item-menu">
-                <img width="50" height="50" src="https://img.icons8.com/ios/50/home--v1.png" alt="home--v1"/>
-                <h2>Sobre</h2>
-            </li>
-        </router-link>
-        </ul>
-    </nav>
 
     <h1 class="title">Banco de Talentos</h1>
 </template>
@@ -38,15 +15,5 @@
     display: flex;
     justify-content: space-between;
     list-style: none;
-}
-.item-menu {
-    display: flex;
-    border: 1px solid black;
-    border-radius: 6px;
-    padding: 5px;
-    align-items: center;
-    justify-content: space-between;
-    gap: 10px;
-    width: 33%;
 }
 </style>
